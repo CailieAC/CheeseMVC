f6337b177267752c00b3667192f94f5c1f5b67c1
CailieAC <cailiecarlile@gmail.com>
2019-04-23 23:07:11 -0500

    Cheese Views

--- CheeseMVC/Controllers/CheeseController.cs
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -8,15 +8,37 @@ namespace CheeseMVC.Controllers
 {
     public class CheeseController : Controller
     {
+        //static means this property is available to any code in the class
+        //static private List<string> Cheeses = new List<string>();
+        static private Dictionary<string, string> Cheeses = new Dictionary<string, string>();
+
         public IActionResult Index()
+        {
+            ViewBag.cheeses = Cheeses;
+            return View();
+        }
+
+        public IActionResult Add()
         {
             return View();
         }
 
+        [HttpPost]
+        [Route("/Cheese/Add")]
+        public IActionResult NewCheese(string name, string description)
+        {
+            //Add the new cheese to my existing cheeses
+            Cheeses.Add(name, description);
+
+            return Redirect("/Cheese");
+        }
+
+        /*
         public IActionResult Index2 ()
         {
             //This will render the Index view above
             return View("Index");
         }
+        */
     }
 }
