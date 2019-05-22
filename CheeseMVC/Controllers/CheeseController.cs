8b40e184563b5ebda44355a77a18514011c1295d
CailieAC <cailiecarlile@gmail.com>
2019-05-21 22:41:16 -0500

    Cheese Models and Model Binding

--- CheeseMVC/Controllers/CheeseController.cs
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CheeseMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CheeseMVC.Controllers
@@ -10,25 +11,41 @@ namespace CheeseMVC.Controllers
     {
         //static means this property is available to any code in the class
         //static private List<string> Cheeses = new List<string>();
-        static private Dictionary<string, string> Cheeses = new Dictionary<string, string>();
+        //static private List<Cheese> Cheeses = new List<Cheese>();
 
         public IActionResult Index()
         {
-            ViewBag.cheeses = Cheeses;
+            ViewBag.cheeses = CheeseData.GetAll();
             return View();
         }
 
         public IActionResult Add()
         {
+
             return View();
         }
 
         [HttpPost]
         [Route("/Cheese/Add")]
-        public IActionResult NewCheese(string name, string description)
+        public IActionResult NewCheese(Cheese newCheese)
         {
-            //Add the new cheese to my existing cheeses
-            Cheeses.Add(name, description);
+            //Add the new cheese to my existing cheeses 
+            /*Model binding means we don't have to create a new cheese like this. But you have to
+             * have a default constructor in the model for model binding to work
+             * Cheese newCheese = new Cheese
+            {
+                Description = description,
+                Name = name
+            };
+            */
+
+            CheeseData.Add(newCheese);
+
+            /*Another way:
+            Cheese newCheese = new Cheese();
+            newCheese.Description = description;
+            newCheese.Name = name;
+            */
 
             return Redirect("/Cheese");
         }
@@ -40,5 +57,22 @@ namespace CheeseMVC.Controllers
             return View("Index");
         }
         */
+
+        public IActionResult Remove()
+        {
+            ViewBag.title = "Remove Cheeses";
+            ViewBag.cheeses = CheeseData.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Remove(int[] cheeseIds)
+        {
+            foreach (int cheeseId in cheeseIds)
+            {
+                CheeseData.Remove(cheeseId);
+            }
+            return Redirect("/Cheese");
+        }
     }
 }
---
+++ CheeseMVC/Models/CheeseData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Models
+{
+    public class CheeseData
+    {
+        static private List<Cheese> cheeses = new List<Cheese>();
+
+        //GetAll Method
+        public static List<Cheese> GetAll()
+        {
+            return cheeses;
+        }
+
+        //Add Method
+        public static void Add(Cheese cheese)
+        {
+            cheeses.Add(cheese);
+        }
+
+        //Remove Method
+        public static void Remove(int id)
+        {
+            cheeses.Remove(GetById(id));
+        }
+
+        //GetById Method
+        public static Cheese GetById(int id)
+        {
+            return cheeses.SingleOrDefault(x => x.CheeseId == id);
+        }
+    }
+}
